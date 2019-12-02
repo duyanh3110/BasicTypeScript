661281a14fefa3eacd76d77cb28ef324a111fdb9
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-12-02 23:50:02 +0200

    interfaces & classes

--- interfaces.ts
+++ interfaces.ts
@@ -1,21 +1,34 @@
 // Interfaces: Creates a new type, describing the
-// property names and vlaues types of an object
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+// property names and values types of an object
+interface Reportable {
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name};`
+  }
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.name}`);
-  console.log(`Broken: ${vehicle.broken}`);
+const user = {
+  name: 'Duy Anh',
+  age: 24,
+  student: true,
+  summary(): string {
+    return `My name is ${this.name}`;
+  }
 };
 
-printVehicle(oldCivic);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(user);
+
+// General Strategy for Reusable Code in TS
+// 1. Create functions that accept arguments that are typed with interfaces
+// 2. Objects/classes can decide to 'implement' a given interface to work with a function
